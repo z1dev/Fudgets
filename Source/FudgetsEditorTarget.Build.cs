1526d31f83a10b1a2a91cbaf6065521dad8bc825
z1dev <zkanjidev@gmail.com>
2024-01-16 06:02:05 +0100

    Initial commit

---
+++ Source/FudgetsEditorTarget.Build.cs
@@ -0,0 +1,14 @@
+﻿using Flax.Build;
+
+public class FudgetsEditorTarget : GameProjectEditorTarget
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        // Reference the modules for editor
+        Modules.Add("Fudgets");
+        Modules.Add("FudgetsEditor");
+    }
+}
