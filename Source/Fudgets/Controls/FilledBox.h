2ae4e7ccbd8f7d0915d071c1b8be44ff38e6dc08
z1dev <zkanjidev@gmail.com>
2024-01-18 02:28:25 +0100

    Hints for the test control

--- Source/Fudgets/Controls/FilledBox.h
+++ Source/Fudgets/Controls/FilledBox.h
@@ -3,6 +3,9 @@
 #include "../Control.h"
 #include "Engine/Core/Math/Color.h"
 
+/// <summary>
+/// Test control that fills its background with color
+/// </summary>
 API_CLASS(NoSpawn)
 class FUDGETS_API FudgetFilledBox : public FudgetControl
 {
@@ -10,8 +13,16 @@ class FUDGETS_API FudgetFilledBox : public FudgetControl
 public:
 	FudgetFilledBox();
 
-	Color GetColor() const { return _color; }
-	void SetColor(Color value) { _color = value; }
+	/// <summary>
+	/// Gets the background color used for filling the control.
+	/// </summary>
+	/// <returns>Background color</returns>
+	API_PROPERTY() Color GetColor() const { return _color; }
+	/// <summary>
+	/// Sets the background color used for filling the control.
+	/// </summary>
+	/// <returns>Background color</returns>
+	API_PROPERTY() void SetColor(Color value) { _color = value; }
 
 	void Draw() override;
 private:
