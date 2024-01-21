4ec074428d2195d19c447e82a788499c58c66cf6
Menotdan <32620310+Menotdan@users.noreply.github.com>
2024-01-21 18:29:10 -0500

    Added more features to the editor window.

--- Source/FudgetsEditor/FudgetsEditorWindow.cs
+++ Source/FudgetsEditor/FudgetsEditorWindow.cs
@@ -1,8 +1,13 @@
 ﻿using FlaxEditor;
+using FlaxEditor.CustomEditors;
+using FlaxEditor.GUI.Tree;
+using FlaxEditor.SceneGraph;
+using FlaxEditor.Viewport;
 using FlaxEditor.Windows;
 using FlaxEngine;
 using FlaxEngine.GUI;
 using Fudgets;
+using System.Collections.Generic;
 using static FlaxEditor.Surface.Archetypes.Animation;
 
 namespace FudgetsEditor;
@@ -12,7 +17,7 @@ namespace FudgetsEditor;
 /// </summary>
 
 // TODO: Make this an asset editor window, and create an asset for Fudgets.
-public class FudgetsEditorWindow : EditorWindow
+public class FudgetsEditorWindow : EditorWindow, IPresenterOwner
 {
     // TODO: Figure out how to (and if you can) simply store a reference to a FudgetGUIRoot
     private Fudget _fudget;
@@ -20,8 +25,22 @@ public class FudgetsEditorWindow : EditorWindow
     private RenderTask _renderTask = null;
     private Editor _editor;
 
+    // Window controls
+    private SplitPanel _panel;
+    private Panel _imagePanel;
+    private Panel _editorRootPanel;
+    private SplitPanel _editorPanel;
+
+    // Editor window controls
+    private Panel _editorTreePanel;
+    private Panel _editorPropertiesPanel;
+    private Tree _tree;
+
     // TODO: Figure out how to properly resize the GPU Texture and whatnot during render time
     private Vector2 _resolution = new Vector2(1920, 1080);
+    private CustomEditorPresenter _fudgetControlsEditor;
+
+    public EditorViewport PresenterViewport => null;
 
     public FudgetsEditorWindow(Editor editor, bool hideOnClose, ScrollBars scrollBars) : base(editor, hideOnClose, scrollBars)
     {
@@ -52,7 +71,35 @@ public class FudgetsEditorWindow : EditorWindow
             _renderTask = null;
         }
 
-        Image image = AddChild<Image>();
+        // Main Panel
+        _panel = AddChild(new SplitPanel());
+        _panel.AnchorPreset = AnchorPresets.StretchAll;
+        _panel.SplitterValue = 0.7f;
+        _imagePanel = _panel.Panel1;
+        _editorRootPanel = _panel.Panel2;
+
+        // Main Split Panels
+        _imagePanel.AnchorPreset = AnchorPresets.StretchAll;
+        _editorRootPanel.AnchorPreset = AnchorPresets.StretchAll;
+
+        // Editor Panel
+        _editorPanel = _editorRootPanel.AddChild(new SplitPanel(orientation: Orientation.Vertical));
+        _editorPanel.AnchorPreset = AnchorPresets.StretchAll;
+        _editorPanel.SplitterValue = 0.7f;
+        _editorTreePanel = _editorPanel.Panel1;
+        _editorPropertiesPanel = _editorPanel.Panel2;
+
+        // Editor Split Panels
+        _tree = _editorTreePanel.AddChild<Tree>();
+        _tree.AnchorPreset = AnchorPresets.StretchAll;
+        _tree.SelectedChanged += SelectedControlChanged;
+        OnRebuildTree();
+
+        _fudgetControlsEditor = new CustomEditorPresenter(null, "No control selected.", this); // No undo for knowledge editing
+        _fudgetControlsEditor.Features = FeatureFlags.None;
+        _fudgetControlsEditor.Panel.Parent = _editorPropertiesPanel;
+
+        Image image = _imagePanel.AddChild<Image>();
         image.AnchorPreset = AnchorPresets.StretchAll;
 
         GPUTextureBrush brush = new GPUTextureBrush();
@@ -76,6 +123,11 @@ public class FudgetsEditorWindow : EditorWindow
         image.Brush = brush;
     }
 
+    private void SelectedControlChanged(List<TreeNode> before, List<TreeNode> after)
+    {
+        _fudgetControlsEditor.Select(after[0].Tag);
+    }
+
     // Temporary
     private void ConfigureFudget()
     {
@@ -85,12 +137,54 @@ public class FudgetsEditorWindow : EditorWindow
         FudgetFilledBox box2 = new FudgetFilledBox();
         box2.Color = Color.Green;
 
+        FudgetContainer containerTest = new FudgetContainer();
+        FudgetFilledBox box3 = new FudgetFilledBox();
+        box3.Color = Color.DarkRed;
+        FudgetFilledBox box4 = new FudgetFilledBox();
+        box4.Color = Color.Beige;
+
         box.HintSize = new Float2(9999, 9999);
         box2.HintSize = new Float2(9999, 9999);
+        box3.HintSize = new Float2(9999, 9999);
+        box4.HintSize = new Float2(9999, 9999);
+        containerTest.HintSize = new Float2(9999, 9999);
+
+        containerTest.CreateLayout<FudgetListLayout>();
+        containerTest.AddChild(box3);
+        containerTest.AddChild(box4);
+
+        box.Name = "Box";
+        box2.Name = "Box 2";
+        box3.Name = "Box 3";
+        box4.Name = "Box 4";
+        containerTest.Name = "Container.";
 
         root.CreateLayout<FudgetListLayout>();
         root.AddChild(box);
         root.AddChild(box2);
+        root.AddChild(containerTest);
+    }
+
+    private void OnRebuildTree()
+    {
+        _tree.RemoveChildren();
+        BuildForControl(_fudget.GUI, _tree);
+    }
+
+    private void BuildForControl(FudgetContainer container, ContainerControl currentTreeNode)
+    {
+        for (int i = 0; i < container.ChildCount; i++)
+        {
+            FudgetControl control = container.ChildAt(i);
+            TreeNode node = currentTreeNode.AddChild<TreeNode>();
+            node.Text = control.Name;
+            node.Tag = control;
+
+            if (control is FudgetContainer con)
+            {
+                BuildForControl(con, node);
+            }
+        }
     }
 
     private void RenderWindow(RenderTask task, GPUContext context)
@@ -116,5 +210,9 @@ public class FudgetsEditorWindow : EditorWindow
             Object.Destroy(_renderTask);
             _renderTask = null;
         }
+    }
+
+    public void Select(List<SceneGraphNode> nodes)
+    {
     }
 }
