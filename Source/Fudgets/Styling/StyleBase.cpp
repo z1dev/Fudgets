b06ae5df9c87ea5108e996e910366c4e5eb097ec
z1dev <zkanjidev@gmail.com>
2024-01-29 17:11:04 +0100

    Deleted unused StyleBase.h and cpp.

--- Source/Fudgets/Styling/StyleBase.cpp
+++
@@ -1,6 +0,0 @@
-#include "StyleBase.h"
-
-FudgetStyleBase::FudgetStyleBase(FudgetThemeBase *theme, FudgetToken token) : Base(SpawnParams(Guid::New(), TypeInitializer)), _theme(theme), _token(token)
-{
-
-}
